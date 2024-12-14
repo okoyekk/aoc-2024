95fd8fe3dc227040cc802b1d9fc748e50730723e
Kenechi Okoye <okoyekk@gmail.com>
2024-12-13 20:38:13 -0500

    4

--- 4/4.py
+++ 4/4.py
@@ -9,16 +9,16 @@ def load_input():
                 matrix[index].append(letter)
         return matrix
 
-def find_coordinates(matrix: list[list[chr]]) -> list[tuple[int, int]]:
+def find_coordinates(letter: chr, matrix: list[list[chr]]) -> list[tuple[int, int]]:
     coordinates = []
     for row in range(len(matrix)):
         for col in range(len(matrix[0])):
-            if matrix[row][col] == 'X':
+            if matrix[row][col] == letter:
                 coordinates.append((row, col))
 
     return coordinates
 
-def count_matches(matrix: list[list[chr]], coordinates: list[tuple[int, int]]) -> int:
+def count_xmas_matches(matrix: list[list[chr]], coordinates: list[tuple[int, int]]) -> int:
     rows = len(matrix)
     cols = len(matrix[0])
     matches = 0
@@ -58,16 +58,58 @@ def count_matches(matrix: list[list[chr]], coordinates: list[tuple[int, int]]) -
                 matches += 1
     return matches
 
+def count_cross_matches(matrix: list[list[chr]], coordinates: list[tuple[int, int]]) -> int:
+    rows = len(matrix)
+    cols = len(matrix[0])
+    matches = 0
+    for x, y in coordinates:
+        if not (x + 1 < rows and x - 1 >= 0 and y + 1 < cols and y - 1 >= 0):
+            # There should be 1 block to each direction of the coordinate
+            continue # coordinate is probably on an edge of some kind...
+    # Configurations:
+    # TODO: maybe might need to have a temp var at a certain coord, if == 2 increment and continue since that's a cross...
+        found = 0
+        # M top left and S bottom right??
+        if matrix[x - 1][y - 1] == 'M' and matrix[x + 1][y + 1] == 'S':
+            found += 1
+            if (found == 2):
+                matches += 1
+                continue
+        # M bottom left and S top right
+        if matrix[x - 1][y + 1] == 'M' and matrix[x + 1][y - 1] == 'S':
+            found += 1
+            if (found == 2):
+                matches += 1
+                continue
+        # M top right and S bottom left
+        if matrix[x + 1][y - 1] == 'M' and matrix[x - 1][y + 1] == 'S':
+            found += 1
+            if (found == 2):
+                matches += 1
+                continue
+        # M bottom right and S top left
+        if matrix[x + 1][y + 1] == 'M' and matrix[x - 1][y - 1] == 'S':
+            found += 1
+            if (found == 2):
+                matches += 1
+                continue
+    return matches
+
+
 
 def main():
     data = load_input()
     # pprint.pp(data)
     # Find coordinates of all X's in matrix
     # Explore in both directions - horizontally, vertically, and diagonally for "XMAS"
-    coordinates = find_coordinates(data)
-    # print(coordinates)
+    x_coordinates = find_coordinates('X', data)
+    # print(x_coordinates)
 
-    print(count_matches(data, coordinates))
+    print(count_xmas_matches(data, x_coordinates))
+
+    a_coordinates = find_coordinates('A', data)
+
+    print(count_cross_matches(data, a_coordinates))
 
 
 
